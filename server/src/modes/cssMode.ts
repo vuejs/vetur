930983aa18ae0f276429c061f268405eab3da553
Pine Wu <octref@gmail.com>
2017-01-03 11:35:16 +0800

    Bring in SCSS and LESS language service

--- server/src/modes/cssMode.ts
+++ server/src/modes/cssMode.ts
@@ -1,60 +1,74 @@
 import { LanguageModelCache, getLanguageModelCache } from '../languageModelCache';
 import { TextDocument, Position } from 'vscode-languageserver-types';
-import { getCSSLanguageService, Stylesheet } from 'vscode-css-languageservice';
+import { getCSSLanguageService, getSCSSLanguageService, getLESSLanguageService, Stylesheet, LanguageService } from 'vscode-css-languageservice';
 import { LanguageMode } from './languageModes';
 import { HTMLDocumentRegions, CSS_STYLE_RULE } from './embeddedSupport';
 
 export function getCSSMode(documentRegions: LanguageModelCache<HTMLDocumentRegions>): LanguageMode {
-  let cssLanguageService = getCSSLanguageService();
-  let embeddedCSSDocuments = getLanguageModelCache<TextDocument>(10, 60, document => documentRegions.get(document).getEmbeddedDocument('css'));
-  let cssStylesheets = getLanguageModelCache<Stylesheet>(10, 60, document => cssLanguageService.parseStylesheet(document));
+  const languageService = getCSSLanguageService();
+  return getStyleMode('css', languageService, documentRegions);
+}
+
+export function getSCSSMode(documentRegions: LanguageModelCache<HTMLDocumentRegions>): LanguageMode {
+  const languageService = getSCSSLanguageService();
+  return getStyleMode('scss', languageService, documentRegions);
+}
+export function getLESSMode(documentRegions: LanguageModelCache<HTMLDocumentRegions>): LanguageMode {
+  const languageService = getLESSLanguageService();
+  return getStyleMode('less', languageService, documentRegions);
+}
+
+function getStyleMode(languageId: string, languageService: LanguageService, documentRegions: LanguageModelCache<HTMLDocumentRegions>): LanguageMode {
+
+  const embeddedDocuments = getLanguageModelCache<TextDocument>(10, 60, document => documentRegions.get(document).getEmbeddedDocument(languageId));
+  const stylesheets = getLanguageModelCache<Stylesheet>(10, 60, document => languageService.parseStylesheet(document));
 
   return {
     getId() {
-      return 'css';
+      return languageId;
     },
     configure(options: any) {
-      cssLanguageService.configure(options && options.css);
+      languageService.configure(options && options.css);
     },
     doValidation(document: TextDocument) {
-      let embedded = embeddedCSSDocuments.get(document);
-      return cssLanguageService.doValidation(embedded, cssStylesheets.get(embedded));
+      let embedded = embeddedDocuments.get(document);
+      return languageService.doValidation(embedded, stylesheets.get(embedded));
     },
     doComplete(document: TextDocument, position: Position) {
-      let embedded = embeddedCSSDocuments.get(document);
-      return cssLanguageService.doComplete(embedded, position, cssStylesheets.get(embedded));
+      let embedded = embeddedDocuments.get(document);
+      return languageService.doComplete(embedded, position, stylesheets.get(embedded));
     },
     doHover(document: TextDocument, position: Position) {
-      let embedded = embeddedCSSDocuments.get(document);
-      return cssLanguageService.doHover(embedded, position, cssStylesheets.get(embedded));
+      let embedded = embeddedDocuments.get(document);
+      return languageService.doHover(embedded, position, stylesheets.get(embedded));
     },
     findDocumentHighlight(document: TextDocument, position: Position) {
-      let embedded = embeddedCSSDocuments.get(document);
-      return cssLanguageService.findDocumentHighlights(embedded, position, cssStylesheets.get(embedded));
+      let embedded = embeddedDocuments.get(document);
+      return languageService.findDocumentHighlights(embedded, position, stylesheets.get(embedded));
     },
     findDocumentSymbols(document: TextDocument) {
-      let embedded = embeddedCSSDocuments.get(document);
-      return cssLanguageService.findDocumentSymbols(embedded, cssStylesheets.get(embedded)).filter(s => s.name !== CSS_STYLE_RULE);
+      let embedded = embeddedDocuments.get(document);
+      return languageService.findDocumentSymbols(embedded, stylesheets.get(embedded)).filter(s => s.name !== CSS_STYLE_RULE);
     },
     findDefinition(document: TextDocument, position: Position) {
-      let embedded = embeddedCSSDocuments.get(document);
-      return cssLanguageService.findDefinition(embedded, position, cssStylesheets.get(embedded));
+      let embedded = embeddedDocuments.get(document);
+      return languageService.findDefinition(embedded, position, stylesheets.get(embedded));
     },
     findReferences(document: TextDocument, position: Position) {
-      let embedded = embeddedCSSDocuments.get(document);
-      return cssLanguageService.findReferences(embedded, position, cssStylesheets.get(embedded));
+      let embedded = embeddedDocuments.get(document);
+      return languageService.findReferences(embedded, position, stylesheets.get(embedded));
     },
     findColorSymbols(document: TextDocument) {
-      let embedded = embeddedCSSDocuments.get(document);
-      return cssLanguageService.findColorSymbols(embedded, cssStylesheets.get(embedded));
+      let embedded = embeddedDocuments.get(document);
+      return languageService.findColorSymbols(embedded, stylesheets.get(embedded));
     },
     onDocumentRemoved(document: TextDocument) {
-      embeddedCSSDocuments.onDocumentRemoved(document);
-      cssStylesheets.onDocumentRemoved(document);
+      embeddedDocuments.onDocumentRemoved(document);
+      stylesheets.onDocumentRemoved(document);
     },
     dispose() {
-      embeddedCSSDocuments.dispose();
-      cssStylesheets.dispose();
+      embeddedDocuments.dispose();
+      stylesheets.dispose();
     }
   };
 };
