162354e099ca2bfe7b1ccd3266af7618848a6c52
Pine Wu <octref@gmail.com>
2017-03-13 01:33:38 -0400

    Use 6005 port to avoid conflict with html LS. Fix #88

--- client/src/htmlMain.ts
+++ client/src/htmlMain.ts
@@ -14,7 +14,7 @@ export function activate(context: ExtensionContext) {
   // The server is implemented in node
   let serverModule = context.asAbsolutePath(path.join('client', 'server', 'htmlServerMain.js'));
   // The debug options for the server
-  let debugOptions = { execArgv: ['--nolazy', '--debug=6004'] };
+  let debugOptions = { execArgv: ['--nolazy', '--debug=6005'] };
 
   // If the extension is launch in debug mode the debug server options are use
   // Otherwise the run options are used
