ed82b9b9acccd431dbfc4d790c999b6664a1e963
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-03-17 08:52:14 -0700

    Improve names and explanation of range-setting code
    It's required to make sure that getXXXAtLocation works correctly,
    because if the ranges are missing from the synthetic nodes, the language
    service asserts, and if the ranges are wrong, the language service's AST
    search gets confused.

--- server/src/modes/typescriptMode.ts
+++ server/src/modes/typescriptMode.ts
@@ -53,24 +53,30 @@ function modifyVueSource(sourceFile: ts.SourceFile): void {
     (st as ts.ExportAssignment).expression.kind === ts.SyntaxKind.ObjectLiteralExpression);
   if (exportDefaultObject) {
     // 1. add `import Vue from './vue'
-    //       (the span of the statement is (0,0) to avoid overlapping existing statements)
-    const zero = <T extends ts.Node>(n: T) => ts.setTextRange(n, { pos: 0, end: 0 });
-    const vueImport = zero(ts.createImportDeclaration(undefined,
+    //    (the span of the inserted statement must be (0,0) to avoid overlapping existing statements)
+    const setZeroPos = getWrapperRangeSetter({ pos: 0, end: 0 });
+    const vueImport = setZeroPos(ts.createImportDeclaration(undefined,
       undefined,
-      zero(ts.createImportClause(undefined,
-        zero(ts.createNamedImports([
-          zero(ts.createImportSpecifier(
-            zero(ts.createIdentifier('Vue')),
-            zero(ts.createIdentifier('Vue'))))])))),
-      zero(ts.createLiteral('vue'))));
+      setZeroPos(ts.createImportClause(undefined,
+        setZeroPos(ts.createNamedImports([
+          setZeroPos(ts.createImportSpecifier(
+            setZeroPos(ts.createIdentifier('Vue')),
+            setZeroPos(ts.createIdentifier('Vue'))))])))),
+      setZeroPos(ts.createLiteral('vue'))));
     sourceFile.statements.unshift(vueImport);
 
     // 2. find the export default and wrap it in `new Vue(...)` if it exists and is an object literal
-    //       (the span of the construct call is the same as the object literal)
+    //    (the span of the wrapping construct call and *all* its members must be the same as the object literal it wraps)
     const objectLiteral = (exportDefaultObject as ts.ExportAssignment).expression as ts.ObjectLiteralExpression;
-    const o = <T extends ts.TextRange>(n: T) => ts.setTextRange(n, objectLiteral);
+    const setObjPos = getWrapperRangeSetter(objectLiteral);
     const vue = ts.setTextRange(ts.createIdentifier('Vue'), { pos: objectLiteral.pos, end: objectLiteral.pos + 1 });
-    (exportDefaultObject as ts.ExportAssignment).expression = o(ts.createNew(vue, undefined, [objectLiteral]));
-    o(((exportDefaultObject as ts.ExportAssignment).expression as ts.NewExpression).arguments);
+    (exportDefaultObject as ts.ExportAssignment).expression = setObjPos(ts.createNew(vue, undefined, [objectLiteral]));
+    setObjPos(((exportDefaultObject as ts.ExportAssignment).expression as ts.NewExpression).arguments);
   }
+}
+
+/** Create a function that calls setTextRange on synthetic wrapper nodes that need a valid range */
+function getWrapperRangeSetter(wrapped: ts.TextRange): <T extends ts.TextRange>(wrapperNode: T) => T {
+  return <T extends ts.TextRange>(wrapperNode: T) => ts.setTextRange(wrapperNode, wrapped);
+
 }
