659fe6698ea311674a8fb053601c28c7082ce740
Pine Wu <octref@gmail.com>
2017-06-02 16:30:58 -0400

    Do not duplicate readme and chagnelog for doc

--- build/update-docs.sh
+++ build/update-docs.sh
@@ -1,3 +1,5 @@
+cp README.md CHANGELOG.md ./docs/
+
 cd docs
 rm -rf _book
 gitbook install
@@ -7,3 +9,6 @@ git init
 git add -A
 git commit -m 'update book'
 git push -f git@github.com:octref/vetur.git master:gh-pages
+
+cd ..
+rm ./CHANGELOG.md ./README.md
