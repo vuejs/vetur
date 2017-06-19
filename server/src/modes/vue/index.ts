80d509eeba63ab7ee016d7c703f6fb9371ac553f
Pine Wu <octref@gmail.com>
2017-06-19 22:24:58 +0200

    Split out vue mode and workaround Microsoft/vscode#29047

---
+++ server/src/modes/vue/index.ts
@@ -0,0 +1,15 @@
+import { LanguageMode } from '../languageModes';
+import { doScaffoldComplete } from './scaffoldCompletion';
+
+export function getVueMode(): LanguageMode {
+  return {
+    getId() {
+      return 'vue';
+    },
+    doComplete (document, position) {
+      return doScaffoldComplete();
+    },
+    onDocumentRemoved() {},
+    dispose() {}
+  };
+}
