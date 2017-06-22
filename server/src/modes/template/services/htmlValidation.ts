e457a103b51f9760d4644b879cbd514fe0ae11e6
Pine Wu <octref@gmail.com>
2017-06-22 19:36:50 +0200

    Guard against null and remove double unequal. Fix #258

--- server/src/modes/template/services/htmlValidation.ts
+++ server/src/modes/template/services/htmlValidation.ts
@@ -5,14 +5,14 @@ import { TextDocument, Diagnostic, Range, DiagnosticSeverity } from 'vscode-lang
 function toDiagnostic(error: ESLintError): Diagnostic {
   const line = error.line - 1;
   const column = error.column - 1;
-  const endLine = error.endLine != null ? error.endLine - 1 : line;
-  const endColumn = error.endColumn != null ? error.endColumn - 1 : column;
+  const endLine = error.endLine ? error.endLine - 1 : line;
+  const endColumn = error.endColumn ? error.endColumn - 1 : column;
   return {
     range: Range.create(line, column, endLine, endColumn),
     message: error.message,
     source: 'vue-language-server',
     severity: error.severity === 1 ? DiagnosticSeverity.Warning : DiagnosticSeverity.Error
-  }
+  };
 }
 
 export function doValidation(document: TextDocument, engine: CLIEngine): Diagnostic[] {
@@ -20,7 +20,10 @@ export function doValidation(document: TextDocument, engine: CLIEngine): Diagnos
     .replace(/^\s{10}/, '<template>')
     .replace(/\s{11}$/, '</template>');
   const report = engine.executeOnText(text, document.uri);
-  return report.results[0].messages.map(toDiagnostic);
+
+  return report.results[0]
+    ? report.results[0].messages.map(toDiagnostic)
+    : [];
 }
 
 export function createLintEngine() {
