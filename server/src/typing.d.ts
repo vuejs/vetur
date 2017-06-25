0158b889036a7462596baeac02088700c26f37b7
Herrington Darkholme <nonamesheep1@gmail.com>
2017-06-25 15:00:02 +0800

    clean up dependency and typing

--- server/src/typing.d.ts
+++ server/src/typing.d.ts
@@ -1,34 +1,3 @@
-declare module 'vue-template-compiler' {
-  interface SFCCustomBlock {
-    type: string;
-    content: string;
-    start?: number;
-    end?: number;
-    src?: string;
-    attrs: {[attribute:string]: string};
-  }
-
-  interface SFCBlock {
-    type: string;
-    content: string;
-    start?: number;
-    end?: number;
-    lang?: string;
-    src?: string;
-    scoped?: boolean;
-    module?: string | boolean;
-  }
-
-  interface SFCDescriptor {
-    template?: SFCBlock;
-    script?: SFCBlock;
-    styles: SFCBlock[];
-    customBlocks: SFCCustomBlock[];
-  }
-
-  export function parseComponent(text: string, option: {pad: 'line' | 'space'}): SFCDescriptor;
-}
-
 declare module 'eslint' {
   export interface ESLintError {
       ruleId: string;
