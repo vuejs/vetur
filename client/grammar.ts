71f71fc027d8dd607fea409f6c74fa5e55a10e7a
Pine Wu <octref@gmail.com>
2018-06-06 15:30:47 -0700

    Fix #812

--- client/grammar.ts
+++ client/grammar.ts
@@ -7,7 +7,6 @@ import * as fs from 'fs';
 // Available grammar scopes
 const SCOPES: { [lang: string]: string } = {
   html: 'text.html.basic',
-  jade: 'text.jade',
   pug: 'text.pug',
   haml: 'text.haml',
   css: 'source.css',
