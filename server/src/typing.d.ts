110246135c2fe79b7e70e2cf63f0022edb86311c
ktsn <ktsn55@gmail.com>
2018-12-21 10:28:28 +0800

    use @types/eslint

--- server/src/typing.d.ts
+++ server/src/typing.d.ts
@@ -1,26 +1,3 @@
-declare module 'eslint' {
-  export interface ESLintError {
-    ruleId: string;
-    severity: number;
-    message: string;
-    line: number;
-    column: number;
-    nodeType: string;
-    source: string;
-    endLine?: number;
-    endColumn?: number;
-  }
-  export interface Report {
-    results: {
-      messages: ESLintError[];
-    }[];
-  }
-  export class CLIEngine {
-    constructor(config: any);
-    executeOnText(text: string, filename: string): Report;
-  }
-}
-
 declare module 'eslint-plugin-vue';
 declare module 'parse-gitignore';
 declare module '*.json';
