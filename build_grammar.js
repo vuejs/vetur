380d8587664b1eefea3cb7332b989c371507b7f3
Patrick <patrick.kuen11@gmail.com>
2019-02-07 22:47:05 +0100

    Added convenient js file to build syntaxes we can now stop using the sublime text parser as described in the official docs here: https://code.visualstudio.com/api/language-extensions

---
+++ build_grammar.js
@@ -0,0 +1,13 @@
+const glob = require('glob');
+const jsYaml = require('js-yaml')
+const fs = require('fs')
+const path = require('path')
+
+glob(__dirname + '/syntaxes/*.yaml', {nocase: true}, (err, files) => {
+
+  for (const file of files) {
+    const pathData = path.parse(file)
+    fs.writeFileSync(pathData.dir + '/' + pathData.name + '.tmLanguage.json', JSON.stringify(jsYaml.safeLoad(fs.readFileSync(file))))
+  }
+  console.log('files', files)
+})
