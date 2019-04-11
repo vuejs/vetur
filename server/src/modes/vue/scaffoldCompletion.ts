06403c888032074bb461914d3889992d3827188d
Julian Krieger <jkrieger@htwsaar.de>
2019-04-11 07:39:36 +0200

    Update scaffoldCompletion.ts
    add watch to import

--- server/src/modes/vue/scaffoldCompletion.ts
+++ server/src/modes/vue/scaffoldCompletion.ts
@@ -73,7 +73,7 @@ export default Vue.extend({
       kind: CompletionItemKind.Snippet,
       insertTextFormat: InsertTextFormat.Snippet,
       insertText: `<script lang="ts">
-import { Vue, Component, Prop } from "vue-property-decorator";
+import { Vue, Component, Prop, Watch } from "vue-property-decorator";
 
 export default class $(0) extends Vue{
 \t\
