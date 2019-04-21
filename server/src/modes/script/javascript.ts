029776467bbf834f53ba18d6353a9db96d404eff
Pine Wu <octref@gmail.com>
2019-04-21 08:50:03 -0700

    Some util for source mapping

--- server/src/modes/script/javascript.ts
+++ server/src/modes/script/javascript.ts
@@ -652,6 +652,62 @@ function convertRange(document: TextDocument, span: ts.TextSpan): Range {
   return Range.create(startPosition, endPosition);
 }
 
+/**
+ * Map an offset from actual .vue file to .vue.template file
+ */
+function mapToOffset(document: TextDocument, offset: number, sourceMap: TemplateSourceMap): number {
+  const filePath = getFileFsPath(document.uri);
+
+  for (const sourceNode of sourceMap[filePath]) {
+    if (offset >= sourceNode.from.start && offset <= sourceNode.from.end) {
+      return sourceNode.to.start + (offset - sourceNode.from.start);
+    }
+  }
+
+  // Handle the case when no original range can be mapped
+  return -1;
+}
+
+/**
+ * Map a range from actual .vue file to .vue.template file
+ */
+function mapToRange(document: TextDocument, from: ts.TextSpan, sourceMap: TemplateSourceMap): Range {
+  const filePath = getFileFsPath(document.uri);
+  for (const sourceNode of sourceMap[filePath]) {
+    if (from.start >= sourceNode.from.start && from.start + from.length <= sourceNode.from.end) {
+      const start = sourceNode.to.start + (from.start - sourceNode.from.start);
+      const end = sourceNode.to.end + (from.start + from.length - sourceNode.from.end);
+      return {
+        start: document.positionAt(start),
+        end: document.positionAt(end)
+      };
+    }
+  }
+
+  // Handle the case when no original range can be mapped
+  return Range.create(0, 0, 0, 0);
+}
+
+/**
+ * Map a range from virtual `.vue.template` file back to original file
+ */
+function mapBackRange(document: TextDocument, to: ts.TextSpan, sourceMap: TemplateSourceMap): Range {
+  const filePath = getFileFsPath(document.uri);
+  for (const sourceNode of sourceMap[filePath]) {
+    if (to.start >= sourceNode.to.start && to.start + to.length <= sourceNode.to.end) {
+      const start = sourceNode.from.start + (to.start - sourceNode.to.start);
+      const end = sourceNode.from.end + (to.start + to.length - sourceNode.to.end);
+      return {
+        start: document.positionAt(start),
+        end: document.positionAt(end)
+      };
+    }
+  }
+
+  // Handle the case when no original range can be mapped
+  return Range.create(0, 0, 0, 0);
+}
+
 function convertKind(kind: ts.ScriptElementKind): CompletionItemKind {
   switch (kind) {
     case 'primitive type':
