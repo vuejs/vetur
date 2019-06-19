dec37db59f0707b21191f918dcd4aab94d8fd4ca
yoyo930021 <yoyo930021@gmail.com>
2019-06-19 22:47:12 +0800

    Add integration tests for modifiers completion

--- test/lsp/completion/template.test.ts
+++ test/lsp/completion/template.test.ts
@@ -26,6 +26,46 @@ describe('Should autocomplete for <template>', () => {
     it('completes imported components', async () => {
       await testCompletion(templateDocUri, position(2, 6), ['item']);
     });
+
+    it('completes event modifiers when attribute startsWith @', async () => {
+      await testCompletion(templateDocUri, position(3, 17), ['stop', 'prevent', 'capture']);
+    });
+
+    it('completes event modifiers when attribute startsWith v-on', async () => {
+      await testCompletion(templateDocUri, position(4, 21), ['stop', 'prevent', 'capture']);
+    });
+
+    it('completes key modifiers when keyEvent', async () => {
+      await testCompletion(templateDocUri, position(5, 21), ['enter', 'space', 'right']);
+    });
+
+    it('completes system modifiers when keyEvent', async () => {
+      await testCompletion(templateDocUri, position(6, 26), ['ctrl', 'shift', 'exact']);
+    });
+
+    it('completes mouse modifiers when MouseEvent', async () => {
+      await testCompletion(templateDocUri, position(7, 19), ['left', 'right', 'middle']);
+    });
+
+    it('completes system modifiers when MouseEvent', async () => {
+      await testCompletion(templateDocUri, position(8, 25), ['ctrl', 'shift', 'exact']);
+    });
+
+    it('completes prop modifiers when attribute startsWith :', async () => {
+      await testCompletion(templateDocUri, position(9, 17), ['sync']);
+    });
+
+    it('completes prop modifiers when attribute startsWith v-bind', async () => {
+      await testCompletion(templateDocUri, position(10, 23), ['sync']);
+    });
+
+    it('completes vModel modifiers when attribute startsWith v-model', async () => {
+      await testCompletion(templateDocUri, position(11, 19), ['lazy', 'number', 'trim']);
+    });
+
+    it('completes modifiers when have attribute value', async () => {
+      await testCompletion(templateDocUri, position(12, 19), ['stop', 'prevent', 'capture']);
+    });
   });
 
   describe('Should complete element-ui components', () => {
--- test/lsp/fixture/client/completion/template/Item.vue
+++ test/lsp/fixture/client/completion/template/Item.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-
+  props: ['title']
 }
 </script>
 
