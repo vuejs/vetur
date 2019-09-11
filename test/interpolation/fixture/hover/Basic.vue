4ac5a11494f56aca802ab65ed23f6a2c8455426e
ktsn <ktsn55@gmail.com>
2019-09-11 23:36:23 +0800

    Add test cases for interpolation hover

---
+++ test/interpolation/fixture/hover/Basic.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+
+    <ul>
+      <li v-for="(item, i) in list" :key="i">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Vetur means "Winter" in icelandic.',
+      list: [0, 1, 2]
+    }
+  }
+}
+</script>
