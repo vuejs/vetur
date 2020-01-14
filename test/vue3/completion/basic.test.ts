0f55a57d65b51d6a1e379fd53165b1ca486379a9
Pine Wu <octref@gmail.com>
2020-01-14 09:30:21 +0100

    Integration test for vue3

---
+++ test/vue3/completion/basic.test.ts
@@ -0,0 +1,26 @@
+import { activateLS, showFile, sleep, FILE_LOAD_SLEEP_TIME } from '../../lsp/helper';
+import { position, getDocUri } from '../util';
+import { testCompletion } from './helper';
+import { CompletionItemKind } from 'vscode';
+
+describe('Vue 3 integration test', () => {
+  const fileUri = getDocUri('completion/Basic.vue');
+
+  before('activate', async () => {
+    await activateLS();
+    await showFile(fileUri);
+    await sleep(FILE_LOAD_SLEEP_TIME);
+    await sleep(FILE_LOAD_SLEEP_TIME);
+  });
+
+  describe('Should complete Vue 3 options', () => {
+    it('complete `setup`', async () => {
+      await testCompletion(fileUri, position(6, 2), [
+        {
+          label: 'setup',
+          kind: CompletionItemKind.Property
+        }
+      ]);
+    });
+  });
+});
