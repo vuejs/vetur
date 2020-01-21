a75265504c67ad049d6c345c07b9c8ac8723e394
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-01-21 13:16:23 +0900

    Fix Windows paths.
    vscode-uri use lower-case drive-letter. So convert it always in getFilePath()

--- server/src/utils/paths.ts
+++ server/src/utils/paths.ts
@@ -50,8 +50,10 @@ export function getFileFsPath(documentUri: string): string {
 export function getFilePath(documentUri: string): string {
   const IS_WINDOWS = platform() === 'win32';
   if (IS_WINDOWS) {
-    // Windows have a leading slash like /C:/Users/pine
-    return Uri.parse(documentUri).path.slice(1);
+	// Windows have a leading slash like /C:/Users/pine
+    // vscode-uri use lower-case drive letter
+    // https://github.com/microsoft/vscode-uri/blob/95e03c06f87d38f25eda1ae3c343fe5b7eec3f52/src/index.ts#L1017
+    return Uri.parse(documentUri).path.replace(/^\/[a-zA-Z]/, (s: string) => s.slice(1).toLowerCase());
   } else {
     return Uri.parse(documentUri).path;
   }
