71f555ce58759809f0fbc3d0ff164488ed726b38
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-01-21 13:33:09 +0900

    Fix indentation

--- server/src/utils/paths.ts
+++ server/src/utils/paths.ts
@@ -50,7 +50,7 @@ export function getFileFsPath(documentUri: string): string {
 export function getFilePath(documentUri: string): string {
   const IS_WINDOWS = platform() === 'win32';
   if (IS_WINDOWS) {
-	// Windows have a leading slash like /C:/Users/pine
+    // Windows have a leading slash like /C:/Users/pine
     // vscode-uri use lower-case drive letter
     // https://github.com/microsoft/vscode-uri/blob/95e03c06f87d38f25eda1ae3c343fe5b7eec3f52/src/index.ts#L1017
     return Uri.parse(documentUri).path.replace(/^\/[a-zA-Z]/, (s: string) => s.slice(1).toLowerCase());
