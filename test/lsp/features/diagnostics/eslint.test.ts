d5f720055aa295c94c34949b1f017531a6399d1d
Pine Wu <octref@gmail.com>
2020-07-29 16:42:02 +0800

    valid-template-root -> no-multiple-template-root

--- test/lsp/features/diagnostics/eslint.test.ts
+++ test/lsp/features/diagnostics/eslint.test.ts
@@ -18,20 +18,20 @@ describe('Should find diagnostics using eslint-plugin-vue', () => {
         severity: vscode.DiagnosticSeverity.Error,
         message: "\n[vue/require-v-for-key]\nElements in iteration expect to have 'v-bind:key' directives.",
         range: sameLineRange(2, 4, 23),
-        source: 'eslint-plugin-vue',
+        source: 'eslint-plugin-vue'
       },
       {
         severity: vscode.DiagnosticSeverity.Error,
         message: "\n[vue/no-unused-vars]\n'i' is defined but never used.",
         range: sameLineRange(2, 15, 16),
-        source: 'eslint-plugin-vue',
+        source: 'eslint-plugin-vue'
       },
       {
         severity: vscode.DiagnosticSeverity.Error,
-        message: '\n[vue/valid-template-root]\nThe template root requires exactly one element.',
+        message: '\n[vue/no-multiple-template-root]\nThe template root requires exactly one element.',
         range: sameLineRange(6, 2, 13),
-        source: 'eslint-plugin-vue',
-      },
+        source: 'eslint-plugin-vue'
+      }
     ];
 
     await testDiagnostics(docUri, expectedDiagnostics);
