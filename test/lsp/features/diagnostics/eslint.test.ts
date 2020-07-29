1cec65e0f351e5e90873fad6d288e53a6bca7435
Pine Wu <octref@gmail.com>
2020-07-29 17:36:14 +0800

    Fix failing test for #2067

--- test/lsp/features/diagnostics/eslint.test.ts
+++ test/lsp/features/diagnostics/eslint.test.ts
@@ -15,19 +15,19 @@ describe('Should find diagnostics using eslint-plugin-vue', () => {
     const expectedDiagnostics: vscode.Diagnostic[] = [
       {
         severity: vscode.DiagnosticSeverity.Error,
-        message: "\n[vue/require-v-for-key]\nElements in iteration expect to have 'v-bind:key' directives.",
+        message: "[vue/require-v-for-key]\nElements in iteration expect to have 'v-bind:key' directives.",
         range: sameLineRange(2, 4, 23),
         source: 'eslint-plugin-vue'
       },
       {
         severity: vscode.DiagnosticSeverity.Error,
-        message: "\n[vue/no-unused-vars]\n'i' is defined but never used.",
+        message: "[vue/no-unused-vars]\n'i' is defined but never used.",
         range: sameLineRange(2, 15, 16),
         source: 'eslint-plugin-vue'
       },
       {
         severity: vscode.DiagnosticSeverity.Error,
-        message: '\n[vue/no-multiple-template-root]\nThe template root requires exactly one element.',
+        message: '[vue/no-multiple-template-root]\nThe template root requires exactly one element.',
         range: sameLineRange(6, 2, 13),
         source: 'eslint-plugin-vue'
       }
