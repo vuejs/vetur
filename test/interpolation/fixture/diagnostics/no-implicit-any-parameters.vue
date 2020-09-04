2ab8a81c7b338c55fc7b03f9ef52b8f3e4157503
yoyo930021 <yoyo930021@gmail.com>
2020-09-04 22:12:31 +0800

    Test for #1790

---
+++ test/interpolation/fixture/diagnostics/no-implicit-any-parameters.vue
@@ -0,0 +1,17 @@
+<template>
+  <div @click="(event) => onClick(event)">
+    <div @click="(event) => onClick(event)">
+      <div :allow="(event) => onClick(event)" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick (e) {
+      console.log(e)
+    }
+  }
+}
+</script>
---
+++ test/interpolation/fixture/diagnostics/no-implicit-any-v-for-array.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div
+      v-for="num in list"
+      :key="num.q"
+    >
+      Test {{num.q.toString()}}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  computed: {
+    list () {
+      const result = []
+      for (let i = 1; i < 10; i++) {
+        if (i % 2 === 0)
+        result.push({ c: i, q: i })
+      }
+      return result
+    }
+  }
+})
+</script>
