e289f0ae1a3cef0c44f2ea22d6de5213af4ea563
Pine Wu <octref@gmail.com>
2020-09-13 13:50:17 +0800

    Bring back CI

--- test/codeTestRunner.ts
+++ test/codeTestRunner.ts
@@ -25,6 +25,8 @@ async function run(execPath: string, testWorkspaceRelativePath: string, mochaArg
 
   return await runTests({
     vscodeExecutablePath: execPath,
+    // https://github.com/microsoft/vscode/issues/106569
+    version: '1.48.2',
     extensionDevelopmentPath: EXT_ROOT,
     extensionTestsPath: extTestPath,
     extensionTestsEnv: mochaArgs,
