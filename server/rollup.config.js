72bdd7eb6f528c7b07c7521bd19b76f399c44ff7
yoyo930021 <yoyo930021@gmail.com>
2020-09-23 21:22:28 +0800

    Fix copy file when windows build

--- server/rollup.config.js
+++ server/rollup.config.js
@@ -1,11 +1,39 @@
+import fs from 'fs-extra';
+import path from 'path';
+import fg from 'fast-glob';
 import { getRootURL, clearDist, external, onwarn, createPlugins } from '../build/rollup-common-config';
 import { linkVlsInCLI, ignorePartSourcemap } from '../build/rollup-plugins.js';
 import vlsPkg from './package.json';
-import copy from 'rollup-plugin-copy';
 
 const getVlsURL = getRootURL('server');
 
 clearDist(getVlsURL('dist'));
+
+function copySnippets() {
+  return {
+    name: 'copy-snippets',
+    buildEnd() {
+      fs.copySync(
+        path.resolve(__dirname, '../', getVlsURL('src/modes/vue/veturSnippets')),
+        path.resolve(__dirname, '../', getVlsURL('dist/veturSnippets')),
+        { overwrite: true }
+      );
+    }
+  };
+}
+
+function copyTSDefaultLibs() {
+  return {
+    name: 'copy-ts-default-libs',
+    buildEnd() {
+      const files = fg.sync(getVlsURL('node_modules/typescript/lib/lib*.d.ts'), { unique: true, absolute: true });
+      files.forEach(file =>
+        fs.copySync(file, path.resolve(__dirname, '../', getVlsURL('dist/'), path.basename(file)), { overwrite: true })
+      );
+    }
+  };
+}
+
 export default [
   // vls
   {
@@ -16,19 +44,7 @@ export default [
     watch: {
       include: getVlsURL('**')
     },
-    plugins: [
-      ignorePartSourcemap(),
-      ...createPlugins(getVlsURL('tsconfig.json')),
-      copy({
-        targets: [
-          // Vetur built-in snippets
-          { src: getVlsURL('src/modes/vue/veturSnippets'), dest: getVlsURL('dist/') },
-          // TypeScript Default lib files
-          { src: getVlsURL('node_modules/typescript/lib/lib*.d.ts'), dest: getVlsURL('dist/') }
-        ],
-        overwrite: true
-      })
-    ]
+    plugins: [ignorePartSourcemap(), ...createPlugins(getVlsURL('tsconfig.json')), copySnippets(), copyTSDefaultLibs()]
   },
   // vueServerMain
   {
