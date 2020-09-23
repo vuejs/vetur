bf662eec53a4e98e45428d1d2dc12a132c4915d8
yoyo930021 <yoyo930021@gmail.com>
2020-09-24 00:16:58 +0800

    Fix copy-ts-default-libs plugin

--- server/rollup.config.js
+++ server/rollup.config.js
@@ -25,7 +25,11 @@ function copyTSDefaultLibs() {
   return {
     name: 'copy-ts-default-libs',
     buildEnd() {
-      const files = fg.sync(getVlsURL('node_modules/typescript/lib/lib*.d.ts'), { unique: true, absolute: true });
+      const files = fg.sync('node_modules/typescript/lib/lib*.d.ts', {
+        cwd: getVlsURL(''),
+        unique: true,
+        absolute: true
+      });
       files.forEach(file => fs.copySync(file, getVlsURL('dist/' + path.basename(file)), { overwrite: true }));
     }
   };
