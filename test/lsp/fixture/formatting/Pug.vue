3b2be53042d3619e3718c1980404344100655a51
Pine Wu <octref@gmail.com>
2020-10-06 12:26:09 +0800

    Fix #2347

--- test/lsp/fixture/formatting/Pug.vue
+++ test/lsp/fixture/formatting/Pug.vue
@@ -1,11 +1,14 @@
 <template lang="pug">
-- var friends = 10;
+  - var friends =10
 
 case friends
-  when 0
+  when   0
     p you have no friends
   when 1
-    p you have a friend
+    p   you have a friend
   default
     p you have #{friends} friends
+    
+    
+
 </template>
