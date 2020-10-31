e2928a4541243feddc13b616ce3d2d0a6a4d8a87
Pine Wu <octref@gmail.com>
2020-10-31 12:54:34 +0800

    Test and changelog for #1745

---
+++ test/interpolation/fixture/diagnostics/issue-1745-duplicate-event-with-modifiers.vue
@@ -0,0 +1,15 @@
+<template>
+  <input
+   @keydown.down.up.prevent.stop="a"
+   @keydown.esc.prevent="b"
+  />
+</template>
+
+<script>
+export default {
+  methods: {
+    a() {},
+    b() {}
+  }
+}
+</script>
