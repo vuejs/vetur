20c5efcd66a643c85211c7abd8754a0e33f36bfc
yoyo930021 <yoyo930021@gmail.com>
2021-03-07 00:51:16 +0800

    Added new Composition API snippets for vue 2

--- server/src/modes/vue/veturSnippets/script/composition-js-vue2.vue
+++ server/src/modes/vue/veturSnippets/script/composition-js-vue2.vue
@@ -1,5 +1,5 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script>
+import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
 \tsetup() {
--- server/src/modes/vue/veturSnippets/script/composition-ts-vue2.vue
+++ server/src/modes/vue/veturSnippets/script/composition-ts-vue2.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
 \tsetup() {
