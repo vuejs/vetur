afc50150708709f89477c3ace221cda05b84d212
Rafał Chłodnicki <rchl2k@gmail.com>
2021-04-21 22:46:51 +0200

    update some formatting

--- test/interpolation/features/hover/basic.test.ts
+++ test/interpolation/features/hover/basic.test.ts
@@ -7,21 +7,21 @@ describe('Should do hover interpolation for <template>', () => {
 
   it('shows hover for msg in mustache', async () => {
     await testHover(docUri, position(2, 11), {
-      contents: ['\n```ts\n(property) msg: string\n```\n'],
+      contents: ['```ts\n(property) msg: string\n```'],
       range: sameLineRange(2, 10, 13)
     });
   });
 
   it('shows hover for v-for variable', async () => {
     await testHover(docUri, position(5, 20), {
-      contents: ['\n```ts\n(parameter) item: number\n```\n'],
+      contents: ['```ts\n(parameter) item: number\n```'],
       range: sameLineRange(5, 18, 22)
     });
   });
 
   it('shows hover for v-for variable on readonly array', async () => {
     await testHover(docUri, position(10, 20), {
-      contents: ['\n```ts\n(parameter) item: string\n```\n'],
+      contents: ['```ts\n(parameter) item: string\n```'],
       range: sameLineRange(10, 18, 22)
     });
   });
